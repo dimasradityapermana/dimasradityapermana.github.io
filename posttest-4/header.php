401452167fed5d6ec4045332ea17c96d4802cfa5
Dimas Raditya Permana <144466230+dimasradityapermana@users.noreply.github.com>
2024-10-02 16:05:57 +0800

    Add files via upload

---
+++ posttest-4/header.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="left-header">
+        <img src="assets/logo.png" alt="logo">
+        <h2>Dim's Stationery</h2>
+    </div>
+    <nav class="right-header">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about-us.html">About Me</a></li>
+            <li><a href="products.html">Products</a></li>
+            <!-- <li><a href="login.html">Login</a></li> -->
+            <button id="switch-theme">🌓</button>
+        </ul>
+    </nav>
+    <div class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</header>
---
+++ posttest-4/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+    <title>Dim's Stationery</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+
+    
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="process.php" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+
+
+
+    <?php include 'footer.php'; ?>
+    
+    <script src="script.js"></script>
+</body>
+</html>
